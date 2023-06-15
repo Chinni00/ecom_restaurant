1363be983e6afc9444b32e8aba507dd00f0b13b9
chinni00 <chinni0437@gmail>
2023-06-15 22:42:49 +0530

    ecom restaurant template ready

---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import About from "./About"
+import Home from "./Home"
+import Store from "./Store"
+import {Routes,Route,Link} from 'react-router-dom'
+
+
+const Navbar = () => {
+  return (
+    <Routes>
+    <div className="container-fluid text-white bg-dark d-flex justify-content-around p-1" style={{height:'40px'}} >
+    
+     
+    </div>
+    </Routes>
+  )
+}
+
+export default Navbar
