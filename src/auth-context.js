65bdaf237f9b74226e0c1a367c1163e4943e9858
chinni00 <chinni0437@gmail>
2023-06-21 11:14:31 +0530

    login handler changed

---
+++ src/auth-context.js
@@ -0,0 +1,38 @@
+import { createContext, useState } from "react";
+
+
+const AuthContext = createContext({
+  token: "",
+  isLogedIn: false,
+  login: (token) => {},
+  logout: () => {},
+});
+
+export const  AuthContextProvider = (props) => {
+  const initialToken = localStorage.getItem('token')
+  const [token, setToken] = useState(initialToken);
+  
+  const userIsLoggedIn = !!token
+  const loginHandler=(token)=>{
+    setToken(token)
+    localStorage.setItem('token',token)
+    // automatically logout after 5minutes
+    setTimeout(()=>{
+      localStorage.removeItem('token')
+    },300000)
+  }
+  const logoutHandler=()=>{
+    setToken(null)
+    localStorage.removeItem('token')
+    
+  }
+  const contextValue = {
+    token:token,
+    isLogedIn:userIsLoggedIn,
+    login:loginHandler,
+    logout:logoutHandler
+  }
+  return <AuthContext.Provider value={contextValue}>{props.children}</AuthContext.Provider>;
+};
+
+export default AuthContext;
