f5e3d7312c1e0500d60803f53e2ed585b87373cf
chinni00 <chinni0437@gmail>
2023-06-21 12:01:26 +0530

    make cart items visible in cart even refreshed

--- src/auth-context.js
+++ src/auth-context.js
@@ -1,21 +1,30 @@
 import { createContext, useState } from "react";
 
 
+
 const AuthContext = createContext({
   token: "",
+  email:'',
   isLogedIn: false,
-  login: (token) => {},
+  login: (token,email) => {},
   logout: () => {},
 });
 
 export const  AuthContextProvider = (props) => {
   const initialToken = localStorage.getItem('token')
   const [token, setToken] = useState(initialToken);
-  
+  const [userName,setUserName] = useState('');
+ 
+
+
   const userIsLoggedIn = !!token
-  const loginHandler=(token)=>{
+  const loginHandler=(token,email)=>{
     setToken(token)
+    setUserName(email)
+   
     localStorage.setItem('token',token)
+    localStorage.setItem(email,'')
+     
     // automatically logout after 5minutes
     setTimeout(()=>{
       localStorage.removeItem('token')
@@ -23,11 +32,15 @@ export const  AuthContextProvider = (props) => {
   }
   const logoutHandler=()=>{
     setToken(null)
+    setUserName('')
     localStorage.removeItem('token')
+    
+   
     
   }
   const contextValue = {
     token:token,
+    email:userName,
     isLogedIn:userIsLoggedIn,
     login:loginHandler,
     logout:logoutHandler
